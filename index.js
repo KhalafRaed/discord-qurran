34a12b9557c7f251bb70eedbbe1db49ef13a6bf8
Raed Khalaf <raed.k@borngroup.com>
2021-06-05 23:12:42 +0300

    pinger every 5 mins

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client } = require('discord.js')
 const schedule = require('node-schedule');
 const express = require('express');
-require('heroku-self-ping').default(`https://fierce-bayou-48691.herokuapp.com/`, {interval: 5000});
+require('heroku-self-ping').default(`https://fierce-bayou-48691.herokuapp.com/`, {interval: 1000 * 60 * 5});
 
 const path = require('path')
 const PORT = process.env.PORT || 5000
