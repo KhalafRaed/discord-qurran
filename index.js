f466323d30e6c8fae7948ce1de652a04f39a1564
Raed Khalaf <raed.k@borngroup.com>
2021-06-05 11:03:44 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const { Client } = require('discord.js')
+const schedule = require('node-schedule');
+
+const client = new Client();
+
+client.login('ODUwNTk4MjgzMDU5MjY1NTc2.YLsDYg.-qI7i5yGr0HeCdUglykAPWqmmYo');
+
+const CRON_EVERYDAY_AT_10 = '0 10 * * *'
+const DEFAULT_CHANNEL_NAME = '8ra2at'
+
+let job;
+let activePage = 176;
+
+client.once('ready', async () => {
+    const defaultChannel = client.channels.cache.find(channel => channel.name === DEFAULT_CHANNEL_NAME);
+    setup(defaultChannel, CRON_EVERYDAY_AT_10)
+});
+
+client.on('message', (message) => {
+    if (message.author.bot) {
+        return;
+    }
+    if (message.content.indexOf('qurran-configure-page') !== -1) {
+        const msg = message.content.replace('qurran-configure-page', '');
+        try {
+            const newActivePage = Number.parseInt(msg);
+            if (!(newActivePage < 604 && newActivePage > 0)) {
+                message.channel.send("The new page count should be within [604, 0]")
+                console.error("ERROR invalid range")
+            } else {
+                activePage = newActivePage;
+                console.log("SUCCESS")
+                message.channel.send("Now the active page is " + activePage)
+            }
+        } catch (e) {
+            console.error("ERROR")
+            message.channel.send("The Page # is invalid")
+        }
+    }
+});
+
+const setup = (channel, cronExp) => {
+    job = schedule.scheduleJob(cronExp, function(){
+        channel.send(`https://www.daily-quran.com/static/pages/page-${activePage++}.jpg`)
+        channel.send(`https://www.daily-quran.com/static/pages/page-${activePage++}.jpg`)
+    });
+};
